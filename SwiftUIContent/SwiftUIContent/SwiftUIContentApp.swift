f4fa2f2eaa3ae88ea4c22ec34b62c12822f10b7f
Duni Chand <duni.eica@gmail.com>
2025-02-12 20:34:36 +0530

    initial commit

---
+++ SwiftUIContent/SwiftUIContent/SwiftUIContentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContentApp.swift
+//  SwiftUIContent
+//
+//  Created by Macbook Pro on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
