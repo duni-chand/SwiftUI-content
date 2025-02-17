e10e2071f1e8f48e2ae6090cfb628b205633f662
Duni Chand <duni.eica@gmail.com>
2025-02-17 10:52:07 +0530

    Added Featured Screen and Tabbar in homeScreen

--- SwiftUIContent/SwiftUIContent/ContentView.swift
+++ SwiftUIContent/SwiftUIContent/ContentView.swift
@@ -8,15 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+            case featured
+            case list
+        }
+    
     var body: some View {
-        LandmarkList()
-        //BadgeBackground()
-//        Text("Hello there")
-//            .font(Font.system(size: 32.0))
-//            .fontWeight(.bold)
-//            .multilineTextAlignment(.center)
-//            .clipped()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem{
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
             
+            
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
+        //LandmarkList()
+        //BadgeBackground()
+        //        Text("Hello there")
+        //            .font(Font.system(size: 32.0))
+        //            .fontWeight(.bold)
+        //            .multilineTextAlignment(.center)
+        //            .clipped()
+        
     }
 }
 
---
+++ SwiftUIContent/SwiftUIContent/Views/Categories/CategoryRow.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryRow.swift
+//  SwiftUIContent
+//
+//  Created by Macbook Pro on 17/02/25.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        NavigationLink {
+                                                    LandmarkDetail(landmark: landmark)
+                                                } label: {
+                                                    CategoryItem(landmark: landmark)
+                                                }
+                            
+                    }
+                }.padding(.trailing, 15)
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+        return CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(4))
+        )
+}
